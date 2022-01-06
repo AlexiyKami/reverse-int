2b2fe11e869d664bc50164c4cdad65fd7f0e9e94
AlexiyKami <Leksij01@gmail.com>
2022-01-06 14:26:45 +0300

    feat: add function reverse()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let tempString = "";
+    let num= n.toString();
+    for (let i = 1; i <= num.length; i++) {
+        tempString += num[num.length-i];
+    }
+    let int = parseInt(tempString);
+    return int;
 }
